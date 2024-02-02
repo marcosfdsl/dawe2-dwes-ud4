9917d9155171a5f15f0e6eff5a39e1a45793a51f
MarcosFdS <marcos.fernandez@live.u-tad.com>
2024-02-02 12:47:35 +0100

    ca1

--- manejador.js
+++ manejador.js
@@ -1,8 +1,14 @@
 const path = require('path');
+const fs=require('node:fs');
+const date= new Date();
 
 function root(res, ruta){
     res.sendFile(path.join(__dirname, 'html', ruta));
     console.log("Rutear a : " + ruta);
+    let archivo_accesos= fs.createWriteStream('accesos.txt', {"flags":"a"});
+    archivo_accesos.write(
+        date.toLocaleString() +
+        " " + ruta + "\n");
 }
 
 exports.root = root;
